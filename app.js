2317877083ef0bfee8df9734dcb29ce643cecf12
Pablo Garrido <pablogs9@gmail.com>
2016-07-14 13:43:18 +0200

    Update

--- app.js
+++ app.js
@@ -3,66 +3,86 @@
 // REQUIRES ////////////////////////////////////////////////////////////
 //todo: Require serial package to interface with rotors and radiostation
 var express = require('express');
+var bodyParser = require("body-parser");
+var leftPad = require('left-pad')
+
 var Yaesu = require('./rotors/yaesu.js');
 
 // CONSTs //////////////////////////////////////////////////////////////
 var HOST = "0.0.0.0" //Listen to every IP address
 var PORT = 8002 //Listening port
-var SERIAL_DEVICE = "/dev/rotorspath" // Rotors path
+var SERIAL_DEVICE = "/dev/ttyUSB0" // Rotors path
 
 // APPs ////////////////////////////////////////////////////////////////
 var app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // ROUTES, GET AND POST ////////////////////////////////////////////////
 app.get('/radiostation', function(req, res) {
-  	//Generate a response with radio information (mode, frequecy...)
-  	res.send('GET RADIOSTATION');
-  });
+    //Generate a response with radio information (mode, frequecy...)
+    res.send('GET RADIOSTATION');
+});
 
 app.post('/radiostation', function(req, res) {
-	//Set radio information (mode, frequecy...) available on the HTTP request
+    //Set radio information (mode, frequecy...) available on the HTTP request
 
-	var mode = req.param('mode') ;
-	var freq = req.param('freq');
+    var mode = req.param('mode') ;
+    var freq = req.param('freq');
 
-	if (mode && freq ){
-		res.send(mode + ',' + freq)
-	}else{
-		res.send('Parámetros no definidos')
-	}
+    if (mode && freq) {
+        res.send(mode + ',' + freq)
+    } else {
+        res.send('Parámetros no definidos')
+    }
 
 });
 
 app.get('/rotors', function(req, res) {
-	//Generate a response with elevation and azimuth information
-	var y = new Yaesu(SERIAL_DEVICE);
-	y.open();
-  res.send(y.query());
+    //Generate a response with elevation and azimuth information
+
+    var y = new Yaesu(SERIAL_DEVICE);
+
+    y.getData(function(data){
+        res.json(data);
+    })
+
 
 });
 
 app.post('/rotors', function(req, res) {
-  //Set the elevation and azimuth information available on the HTTP request
+    //Set the elevation and azimuth information available on the HTTP request
+    var elevation = leftPad(parseInt(req.body.ele), 3, 0);
+    var azimuth = leftPad(parseInt(req.body.azi), 3, 0);
 
-  var elevation = req.param('ele');
-  var azimuth = req.param('azi');
-
-  if (elevation && azimuth ){
-		var y = new Yaesu("/dev/rotorspath");
-		y.open();
-	  y.move(elevation,azimuth)
-	  res.send('Pos: ' + y.query());
-	}else{
-		res.send('Parámetros no definidos')
-	}
+    if (elevation != "" && azimuth != "") {
+        var SerialPort = require("serialport");
+        var s = new SerialPort(SERIAL_DEVICE);
+        s.on("open", function() {
+                s.write(new Buffer("W" + azimuth + " " + elevation + "\n", "utf8"), function() {
+                    res.json({
+                        status: "Done"
+                    })
+                    s.close()
+                })
+            })
+            // var y = new Yaesu(SERIAL_DEVICE);
+            // y.open();
+            // y.move(elevation, azimuth)
+            // res.send('Pos: ' + y.query());
+    } else {
+        res.json({
+            status: "Error"
+        })
+    }
 
 });
 
 app.get('/*', function(req, res, next) {
-  	console.log('index')
     next();
-}, function (req, res){
-    res.send('INDEX');
+}, function(req, res) {
+    res.end();
 });
 
 app.listen(PORT, HOST)
--- rotors/yaesu.js
+++ rotors/yaesu.js
@@ -1,48 +1,31 @@
 "use strict"
+var SerialPort = require("serialport");
 
-//TODO: insert module issues: export, serial require, inner package.json
-var Rotors = require('./rotors.js');
+module.exports = function Yaesu(sAddress) {
+    var serialAddress = sAddress;
 
-module.exports = function Yaesu(serialAddress) {
-    var serialDevice;
-    var r;
+    function getData(callback){
+        var s = new SerialPort(serialAddress);
+        s.on("open", function() {
+            s.write(new Buffer("C2\n", "utf8"), function() {
+                var answer = ""
+                s.on("data", function(data) {
+                    answer += data
+                    if (answer.substring(answer.length - 2, answer.length) == "\r\n") {
+                        callback({
+                            status: "Done",
+                            azi: parseInt(answer.split('+')[1]),
+                            ele: parseInt(answer.split('+')[2])
+                        })
+                        s.close()
+                    }
+                })
+            })
+        })
 
-    function open() {
-        //serialDevice = serial.open(ser)
-        //r = Rotors("mv", "rd", serialDevice)
-        
-        r = Rotors("mv", "rd")
-    }
-
-    function close(){
-        serialDevice.close()
-    }
-
-    function move(elevation,azimuth){
-        return r.move(elevation,azimuth);
-    }
-
-    function query(elevation,azimuth){
-        return r.query();
     }
 
     return {
-        open: open,
-        close: close,
-        move: move,
-        query: query
+        getData: getData,
     }
 }
-
-
-/*
-Usage:
-
-var r = Yaesu("/dev/rotorspath")
-r.open()
-r.move(90,145)
-r.query().done(function(answer){
-    console.log(answer)
-})
-r.close()
-*/
