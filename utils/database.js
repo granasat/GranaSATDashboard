5c8d33316902b7d83713deabf54053b8d6cff18f
amnbaza <amnbaza@hotmail.com>
2016-07-20 12:30:28 +0200

    reg_user

--- utils/database.js
+++ utils/database.js
@@ -3,6 +3,23 @@
 //Config
 var config = require('./config.js').config
 
+//PASSPORTJS AND AUTH
+function hashPassword(password, salt) {
+    var hash = crypto.createHash('sha256');
+    hash.update(password);
+    hash.update(salt);
+    return hash.digest('hex');
+}
+
+function createSalt() {
+    var len = 30;
+    return crypto.randomBytes(Math.ceil(len * 3 / 4))
+        .toString('base64') // convert to base64 format
+        .slice(0, len) // return required number of characters
+        .replace(/\+/g, '0') // replace '+' with '0'
+        .replace(/\//g, '0'); // replace '/' with '0'
+}
+
 //Database connection
 var mysql = require('mysql');
 var database = mysql.createConnection({
@@ -38,26 +55,20 @@ module.exports = function DashboardDB(req) {
 		});
 	};
 
-	function reg_user(req) {
+	function reg_user(req,done) {
 		passReqToCallback: true,
-		log("Trying to login: " + username + " from " + (req.headers['x-forwarded-for'] || req.connection.remoteAddress), "warn");
-		database.query('select * from USERS where USER_NAME = ?', username, function(err, rows) {
+		
+		var post = {
+			username: req.body.username,
+			organization: req.body.organization,
+			mail: req.body.mail,
+			password: req.body.password,
+			usertype: req.body.usertype
+		};
+		
+		database.query('INSERT INTO USERS(USER_NAME,USER_ORGANIZATION,USER_MAIL,USER_PASSWORD,USER_TYPE) VALUES ?', post, function(err) {
 			if (err) {
 				log(err);
-				return done(null, false);
-			}
-			if (rows.length != 0) {
-				var user = rows[0];
-				var hash = hashPassword(password, user.USER_PASSWORD.split(":")[0]);
-				if (hash == user.USER_PASSWORD.split(":")[1]) {
-					log("Logged " + username + " from " + (req.headers['x-forwarded-for'] || req.connection.remoteAddress));
-					done(null, user);
-				} else {
-					log("Non valid password for " + username + " from " + (req.headers['x-forwarded-for'] || req.connection.remoteAddress), "error");
-					return done(null, false);
-				}
-			} else {
-				log("Non valid username: " + username + " from " + (req.headers['x-forwarded-for'] || req.connection.remoteAddress), "error");
 				return done(null, false);
 			}
 		});
