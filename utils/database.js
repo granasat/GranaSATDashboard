696e2e06b8d3a32bdeee1dfc5a95bb27ca45ae09
amnbaza <amnbaza@hotmail.com>
2016-07-21 19:34:04 +0200

    Database

--- utils/database.js
+++ utils/database.js
@@ -72,6 +72,7 @@ module.exports = function DashboardDB() {
         req.checkBody('organization', 'Organization is required').notEmpty().isAlpha();
         req.checkBody('email', 'A valid email is required').notEmpty().isEmail();
         req.checkBody('password', 'A valid password is required').notEmpty().len(6, 8);
+        req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
         req.checkBody('usertype', 'A valid type is required').notEmpty().isInt();
 
         var salt = createSalt();
@@ -99,6 +100,92 @@ module.exports = function DashboardDB() {
             }
         });
     };
+    
+    function modUser(req, res) {
+        
+        if(req.body.password == null) { //No modify password.
+            
+            req.checkBody('username', 'Name is required').notEmpty().isAlpha().len(6, 20);
+            req.checkBody('organization', 'Organization is required').notEmpty().isAlpha();
+            req.checkBody('email', 'A valid email is required').notEmpty().isEmail();            
+            req.checkBody('usertype', 'A valid type is required').notEmpty().isInt();
+
+            var post = [
+                [
+                    req.body.username,
+                    req.body.organization,
+                    req.body.mail,
+                    req.body.usertype,
+                    req.body.user_id
+                ]
+            ];
+
+            database.query('UPDATE USERS SET USER_NAME = ?, USER_ORGANIZATION = ?, USER_MAIL = ?, USER_TYPE = ? WHERE USER_ID = ?', [post], function(err) {
+                if (err) {
+                    log(err.toString(), "error");
+                    res.json({
+                        error: "Database error"
+                    })
+                } else {
+                    res.json({
+                        status: "Done"
+                    })
+                }
+            });
+        
+        } else {
+        
+            req.checkBody('username', 'Name is required').notEmpty().isAlpha().len(6, 20);
+            req.checkBody('organization', 'Organization is required').notEmpty().isAlpha();
+            req.checkBody('email', 'A valid email is required').notEmpty().isEmail();
+            req.checkBody('password', 'A valid password is required').notEmpty().len(6, 8);
+            req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+            req.checkBody('usertype', 'A valid type is required').notEmpty().isInt();
+
+            var salt = createSalt();
+
+            var post = [
+                [
+                    req.body.username,
+                    req.body.organization,
+                    req.body.mail,
+                    salt + ":" + hashPassword(req.body.password, salt),
+                    req.body.usertype,
+                    req.body.user_id
+                ]
+            ];
+
+            database.query('UPDATE USERS SET USER_NAME = ?, USER_ORGANIZATION = ?, USER_MAIL = ?, USER_PASSWORD = ?, USER_TYPE = ? WHERE USER_ID = ?', [post], function(err) {
+                if (err) {
+                    log(err.toString(), "error");
+                    res.json({
+                        error: "Database error"
+                    })
+                } else {
+                    res.json({
+                        status: "Done"
+                    })
+                }
+            });
+        }
+    };
+    
+    function deleteUser(req, res) {
+        req.checkBody('user_id', 'User ID').notEmpty().isInt();
+
+        database.query('DELETE FROM USERS WHERE USER_ID = ?', req.body.user_id, function(err) {
+            if (err) {
+                log(err.toString(), "error");
+                res.json({
+                    error: "Database error"
+                })
+            } else {
+                res.json({
+                    status: "Done"
+                })
+            }
+        });
+    };
 
     function deserializeUser(id, done) {
         database.query('select * from USERS where USER_ID = ?', id, function(err, rows) {
@@ -106,11 +193,43 @@ module.exports = function DashboardDB() {
             return done(null, rows[0]);
         });
     }
+    
+    function addSatellite(req, res) {
+        req.checkBody('satname', 'Satellite name is required').notEmpty().isAlpha();
+        req.checkBody('tle', 'TLE is required').notEmpty();
+
+        var salt = createSalt();
+
+        var post = [
+            [
+                req.body.satname,
+                req.body.tle,
+                req.body.rx_freq,
+                req.body.tx_freq
+            ]
+        ];
+
+        database.query('INSERT INTO SATELLITES (SAT_NAME,SAT_TEL,SAT_RX_FREQ,SAT_TX_FREQ) VALUES ?', [post], function(err) {
+            if (err) {
+                log(err.toString(), "error");
+                res.json({
+                    error: "Database error"
+                })
+            } else {
+                res.json({
+                    status: "Done"
+                })
+            }
+        });
+    }; 
 
     return {
         loginConfig: loginConfig,
         login: login,
         signup: signup,
-        deserializeUser: deserializeUser
+        modUser: modUser,
+        deleteUser: deleteUser,
+        deserializeUser: deserializeUser,
+        addSatellite: addSatellite
     }
 };
