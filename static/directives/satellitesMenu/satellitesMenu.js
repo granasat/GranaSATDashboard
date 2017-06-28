7a120fc643063d45b385f8e6faae6ad9b888b965
Fran Acién <amil101ftw@gmail.com>
2017-06-28 16:08:18 +0200

    adding satellite's info to table

--- static/directives/satellitesMenu/satellitesMenu.js
+++ static/directives/satellitesMenu/satellitesMenu.js
@@ -3,9 +3,15 @@
  */
 app.directive('satellitesMenu', function($http, $document) {
     function link(scope, element, attrs) {
+        scope.$watch("logged", function(newValue, oldValue) {           //Execute when the user is logged, call for satellites in db
+            if (newValue == true) {
+                scope.getSatellites().then(function(res) {
+                    console.log(res.data);
+                    scope.availableSatellites = res.data;
+                });
+            }
+        });
     }
-
-
     return {
         link: link,
         templateUrl: 'directives/satellitesMenu/satellitesMenu.html',
