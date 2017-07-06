b4f70c322634ad1c4c3cb2824d35f7c5750a21dd
Fran Acién <amil101ftw@gmail.com>
2017-07-06 15:32:00 +0100

    changing directive structure

--- static/directives/passesRegistration/polarGraphDirective.js
+++ static/directives/passesRegistration/polarGraphDirective.js
@@ -2,72 +2,91 @@
  * Created by amil101 on 3/07/17.
  */
 app.directive('d3Bars', ['d3', function(d3) {
-    return {restrict: 'EA',
-        link: function(scope, element, attrs) {
+    function link(scope, element, attrs) {
+        var conv = (2 * Math.PI) / 360;
+        var subpi = Math.PI / 2;
 
-                var conv = (2*Math.PI)/360;
-                var subpi = Math.PI/2;
+        var data = new Array();
 
-                var data = new Array();
+        scope.satellitePasses[scope.selectedItem].polar.forEach(function (elem) {
+            data.push([elem.azi * conv, ((-((elem.ele) * conv / subpi) + 1) / 2)]);
+        });
 
-                scope.satellitePasses[scope.selectedItem].polar.forEach(function (elem) {
-                    data.push([elem.azi * conv, ((-((elem.ele)*conv/subpi) + 1)/2)]);
-                });
+        var width = 960,
+            height = 500,
+            radius = Math.min(width, height) / 2 - 30;
 
-                var width = 960,
-                    height = 500,
-                    radius = Math.min(width, height) / 2 - 30;
+        var r = d3.scale.linear()
+            .domain([0, .5])
+            .range([0, radius]);
 
-                var r = d3.scale.linear()
-                    .domain([0, .5])
-                    .range([0, radius]);
+        var line = d3.svg.line.radial()
+            .radius(function (d) {
+                return r(d[1]);
+            })
+            .angle(function (d) {
+                return -d[0] + Math.PI / 2;
+            });
 
-                var line = d3.svg.line.radial()
-                    .radius(function(d) { return r(d[1]); })
-                    .angle(function(d) { return -d[0] + Math.PI / 2; });
+        var svg = d3.select(element[0]).append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-                var svg = d3.select(element[0]).append("svg")
-                    .attr("width", width)
-                    .attr("height", height)
-                    .append("g")
-                    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        var gr = svg.append("g")
+            .attr("class", "r axis")
+            .selectAll("g")
+            .data(r.ticks(5).slice(1))
+            .enter().append("g");
 
-                var gr = svg.append("g")
-                    .attr("class", "r axis")
-                    .selectAll("g")
-                    .data(r.ticks(5).slice(1))
-                    .enter().append("g");
+        gr.append("circle")
+            .attr("r", r);
 
-                gr.append("circle")
-                    .attr("r", r);
+        gr.append("text")
+            .attr("y", function (d) {
+                return -r(d) - 4;
+            })
+            .attr("transform", "rotate(15)")
+            .style("text-anchor", "middle")
+            .text(function (d) {
+                return d;
+            });
 
-                gr.append("text")
-                    .attr("y", function(d) { return -r(d) - 4; })
-                    .attr("transform", "rotate(15)")
-                    .style("text-anchor", "middle")
-                    .text(function(d) { return d; });
+        var ga = svg.append("g")
+            .attr("class", "a axis")
+            .selectAll("g")
+            .data(d3.range(0, 360, 30))
+            .enter().append("g")
+            .attr("transform", function (d) {
+                return "rotate(" + -d + ")";
+            });
 
-                var ga = svg.append("g")
-                    .attr("class", "a axis")
-                    .selectAll("g")
-                    .data(d3.range(0, 360, 30))
-                    .enter().append("g")
-                    .attr("transform", function(d) { return "rotate(" + -d + ")"; });
+        ga.append("line")
+            .attr("x2", radius);
 
-                ga.append("line")
-                    .attr("x2", radius);
+        ga.append("text")
+            .attr("x", radius + 6)
+            .attr("dy", ".35em")
+            .style("text-anchor", function (d) {
+                return d < 270 && d > 90 ? "end" : null;
+            })
+            .attr("transform", function (d) {
+                return d < 270 && d > 90 ? "rotate(180 " + (radius + 6) + ",0)" : null;
+            })
+            .text(function (d) {
+                return d + "°";
+            });
 
-                ga.append("text")
-                    .attr("x", radius + 6)
-                    .attr("dy", ".35em")
-                    .style("text-anchor", function(d) { return d < 270 && d > 90 ? "end" : null; })
-                    .attr("transform", function(d) { return d < 270 && d > 90 ? "rotate(180 " + (radius + 6) + ",0)" : null; })
-                    .text(function(d) { return d + "°"; });
+        svg.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", line);
 
-                svg.append("path")
-                    .datum(data)
-                    .attr("class", "line")
-                    .attr("d", line);
+    }
 
-        }};
+    return {
+        restrict: 'EA',
+        link: link
+    }
 }]);
