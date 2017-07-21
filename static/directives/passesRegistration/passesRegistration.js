9ae9a7517859209a33d35ba5921f38dc66e5c543
Fran Acién <amil101ftw@gmail.com>
2017-07-21 12:17:09 +0200

    Fixing problem while user schedule two passes in the same time

--- static/directives/passesRegistration/passesRegistration.js
+++ static/directives/passesRegistration/passesRegistration.js
@@ -31,18 +31,28 @@ app.directive('passesRegistration', function($http, $document) {
         };
 
         scope.setPass = function(pass) {
-            $http({
-                method: 'POST',
-                url: "satellites/passes",
-                data: {
-                    sat : scope.satelliteSelected.RMT_NAME,
-                    id : pass.id
-                }
-            }).then(function(res) {
-                if(res.data.status === "Done")
-                    scope.scheduledPasses.push(pass);
-                    pass.scheduled = true;
+            var problematicPasses = scope.scheduledPasses.filter(function (scheduledPass) {
+                return (new Date(scheduledPass.startDateLocal).getTime() <= new Date(pass.endDateLocal).getTime() && new Date(scheduledPass.endDateLocal).getTime() >= new Date(pass.endDateLocal).getTime()) ||
+                       (new Date(scheduledPass.startDateLocal).getTime() <= new Date(pass.startDateLocal).getTime() && new Date(scheduledPass.endDateLocal).getTime() >= new Date(pass.startDateLocal).getTime()) ||
+                       (new Date(scheduledPass.startDateLocal).getTime() >= new Date(pass.startDateLocal).getTime() && new Date(scheduledPass.endDateLocal).getTime() <= new Date(pass.endDateLocal).getTime());
             });
+
+            if(problematicPasses.length > 0){           //The pass that the user is trying to schedule have conflict with a pass that is already scheduled
+                window.alert("That pass is troubled due to another scheduled pass that occurs in the same space of time");
+            }else{
+                $http({
+                    method: 'POST',
+                    url: "satellites/passes",
+                    data: {
+                        sat : scope.satelliteSelected.RMT_NAME,
+                        id : pass.id
+                    }
+                }).then(function(res) {
+                    if(res.data.status === "Done")
+                        scope.scheduledPasses.push(pass);
+                    pass.scheduled = true;
+                });
+            }
         };
 
     }
