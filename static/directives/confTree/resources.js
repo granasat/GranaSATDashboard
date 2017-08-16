9eb43470be4f71cb282df0098cdf2fa7c4882da2
Fran Acién <amil101ftw@gmail.com>
2017-08-16 12:11:49 +0200

    adding tooltip resources

---
+++ static/directives/confTree/resources.js
@@ -0,0 +1,31 @@
+/**
+ * Created by amil101 on 16/08/17.
+ */
+
+app.controller('ic910', function($scope) {
+    $scope.R = {
+        radio : "asdf",
+        freq : "freq",
+        rf_pw : "RF power",
+        connection : "Connection",
+        from_addr : "From address",
+        to_addr : "To address",
+        timeout : "timeout",
+        delay : "delay",
+        repeats : "repeats",
+        serial_radio : "Hooray",
+        debug_on_send : "Debug",
+        debug : "Debug",
+        port_name : "port",
+        provider : "provider",
+        conn : "Connection",
+        baud_rate : "Baud rate",
+        data_bits : "Data bits",
+        stop_bits : "Stop bits",
+        disable_on_broken_serial : "Disable on broken serial",
+        commands : "commands",
+        send : "send",
+        send_bits : "send bits",
+        send_hex : "send hex"
+    };
+});
