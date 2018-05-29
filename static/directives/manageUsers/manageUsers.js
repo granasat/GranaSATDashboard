dafb2caad87843e1ebef3b9d2a99819ba6cc056e
Antonio10sc <serranoantonio@correo.ugr.es>
2018-05-29 21:55:02 +0300

    añadiendo cambios en gestión de usuarios

--- static/directives/manageUsers/manageUsers.js
+++ static/directives/manageUsers/manageUsers.js
@@ -3,15 +3,24 @@
  */
 app.directive('manageUsers', function($http, $document) {
     function link(scope, element, attrs) {
-        scope.$watch("logged", function(newValue, oldValue) {           //Execute when the user is logged, call for satellites in db
-            if (newValue == true) {
-                scope.getUsers().then(function (res) {
-                    scope.users = res.data;
-                    scope.users.forEach(function (elem) {
-                      elem.edit = false;
-                    })
-                });
-            }
+        scope.$watch("logged", function(newValue, oldValue) {
+
+            scope.$watch("type", function(newValue1, oldValue1) {
+
+                if (newValue1 == 1) {
+
+                    //Execute when the user is logged and type 1, call for users in db
+                    if (newValue == true) {
+                        scope.getUsers().then(function (res) {
+                            scope.users = res.data;
+                            scope.users.forEach(function (elem) {
+                                elem.edit = false;
+                            })
+                        });
+                    }
+
+                }
+        });
         });
 
         scope.refreshUsers = function(){
